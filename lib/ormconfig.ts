03ed6f5876f5d71c476013dceea491dc86812593
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-08-10 13:44:55 +0200

    fix TypeOrm config

--- lib/ormconfig.ts
+++ lib/ormconfig.ts
@@ -3,13 +3,14 @@ import { ConnectionOptions } from "typeorm";
 
 const databaseConfig: ConnectionOptions = {
   ...config.get("database"),
-  entities: [__dirname + "/db/entities/*{.ts,.js}"],
-  migrations: ["/db/migrations/*{.ts,.js}"],
+  entities: [__dirname + "/entities/*{.ts,.js}"],
+  migrations: [__dirname + "/migrations/*{.ts,.js}"],
   migrationsRun: true,
   // Only synchronize the database schema in a development environment
   synchronize: process.env.NODE_ENV === "development",
   cli: {
-    migrationsDir: __dirname + "/db/migrations",
+    entitiesDir: __dirname + "/entities",
+    migrationsDir: __dirname + "/migrations",
   },
 };
 
