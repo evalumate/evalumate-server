c2908db896e91efe4773b60e0cc1e18293ae3f5b
Bjoern Luchterhandt <25661029+bjoluc@users.noreply.github.com>
2019-08-14 19:38:41 +0200

    rename MalformedRequestException

--- lib/middlewares/validation.ts
+++ lib/middlewares/validation.ts
@@ -1,7 +1,7 @@
 import { plainToClass } from "class-transformer";
 import { validate, ValidationError } from "class-validator";
 import * as express from "express";
-import MalformedRequestException from "../exceptions/MalformedRequestException";
+import BadRequestException from "../exceptions/BadRequestException";
 
 function validationMiddleware<T>(
   type: any,
@@ -11,7 +11,7 @@ function validationMiddleware<T>(
     validate(plainToClass(type, req.body), { skipMissingProperties }).then(
       (errors: ValidationError[]) => {
         if (errors.length > 0) {
-          next(new MalformedRequestException()); // TODO add error array to the exception class (or the error array)
+          next(new BadRequestException()); // TODO add error array to the exception class (or the error array)
         } else {
           next();
         }
