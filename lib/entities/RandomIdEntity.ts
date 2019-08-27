4380abdc9871e9742e6e74913ae6c4ddb0666928
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-08-27 10:35:36 +0200

    improve RandomIdEntity performance

--- lib/entities/RandomIdEntity.ts
+++ lib/entities/RandomIdEntity.ts
@@ -1,6 +1,13 @@
 import generateUuid from "nanoid/async";
 import generateUuidWithCustomAlphabet from "nanoid/async/generate";
-import { BaseEntity, BeforeInsert, PrimaryColumn } from "typeorm";
+import {
+  BaseEntity,
+  BeforeInsert,
+  PrimaryColumn,
+  SaveOptions,
+  QueryFailedError,
+  Repository,
+} from "typeorm";
 
 /**
  * An abstract base class for TypeORM entities that expose an `id` primary key column which is a
@@ -21,13 +28,30 @@ export default abstract class RandomIdEntity extends BaseEntity {
     const length = Object.getPrototypeOf(this).constructor.randomIdLength;
     const alphabet = Object.getPrototypeOf(this).constructor.randomIdAlphabet;
 
-    do {
-      if (alphabet) {
-        this.id = await generateUuidWithCustomAlphabet(alphabet, length);
-      } else {
-        this.id = await generateUuid(length);
+    if (alphabet) {
+      this.id = await generateUuidWithCustomAlphabet(alphabet, length);
+    } else {
+      this.id = await generateUuid(length);
+    }
+  }
+
+  save(options?: SaveOptions): Promise<this> {
+    const repository = (this.constructor as any).getRepository();
+
+    const saveWithRetry = (timesLeft: number): Promise<this> => {
+      try {
+        return repository.save(this, options);
+      } catch (error) {
+        if (error instanceof QueryFailedError && this.idExists(this.id) && timesLeft > 0) {
+          // Unique constraint error due to duplicate id. Trying again with a new id, automatically
+          // generated thanks to @BeforeInsert
+          return saveWithRetry(timesLeft - 1);
+        }
+        throw error;
       }
-    } while (await this.idExists(this.id));
+    };
+
+    return saveWithRetry(3); // On duplicate ids, regenerate the id up to three times before failing
   }
 
   protected async idExists(id: string): Promise<Boolean> {
