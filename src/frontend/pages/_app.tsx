92fa5e19afb7803bc970f5222c4f8a1335011539
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-10-10 11:53:06 +0200

    remove deprecated container component

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import NextApp, { Container, AppInitialProps } from "next/app";
+import NextApp, { AppInitialProps } from "next/app";
 import withRedux from "next-redux-wrapper";
 import { initStore } from "../lib/store";
 import { Store } from "redux";
@@ -17,11 +17,9 @@ class App extends NextApp<AppProps> {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     );
   }
 }
