b743ec0489ff29b6b39ef28dc1aa269f4d7bb748
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-10-13 20:14:41 +0200

    implement state-based redirects

---
+++ src/frontend/lib/util/redirect.ts
@@ -0,0 +1,48 @@
+import { UserRole } from "../store/reducers/global";
+import { selectUserRole } from "../store/selectors/global";
+import { selectSession } from "../store/selectors/owner";
+import { ServerResponse } from "http";
+import { Store } from "StoreTypes";
+
+/**
+ * Redirects the client to the given domain-relative location.
+ *
+ * @param location The location to redirect to
+ * @param res If not `null`, the response object will be used to reply with a redirect. Otherwise,
+ * client-side execution is assumed and the `push` method of the next.js router is used.
+ */
+export function redirectTo(location: string, res: ServerResponse) {
+  console.log({ location, res });
+  if (res) {
+    res.writeHead(302, {
+      Location: location,
+    });
+    res.end();
+  } else {
+    require("next/router").default.push(location);
+  }
+}
+
+/**
+ * A function to be used in `getInitialProps` that redirects the client or not, depending on the
+ * given redux store's state and the current page's `pathname`.
+ *
+ * @param store A redux store holding the app's current state
+ * @param pathname The current page's path name, as passed to `getInitialProps`.
+ * @param res The response object for the current request or null, as passed to `getInitialProps`
+ */
+export function redirectIfApplicable(store: Store, pathname: string, res: ServerResponse) {
+  const state = store.getState();
+  const userRole = selectUserRole(state);
+  const session = selectSession(state);
+
+  if (pathname === "/") {
+    switch (userRole) {
+      case UserRole.Owner:
+        if (session) {
+          redirectTo(`/master/${session.id}`, res);
+        }
+        break;
+    }
+  }
+}
--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { makeStore } from "../lib/store";
+import { redirectIfApplicable } from "../lib/util/redirect";
 import withRedux from "next-redux-wrapper";
 import NextApp, { AppInitialProps } from "next/app";
 import React from "react";
@@ -9,6 +10,7 @@ type AppProps = AppInitialProps & { store: Store };
 
 class App extends NextApp<AppProps> {
   static async getInitialProps({ Component, ctx }) {
+    redirectIfApplicable(ctx.store, ctx.pathname, ctx.res);
     return {
       pageProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {},
     };
