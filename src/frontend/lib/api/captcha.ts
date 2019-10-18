e7c93713d176b1cd65ab123bf3aef870fe95f049
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-10-18 22:31:37 +0200

    fix captcha api uri

--- src/frontend/lib/api/captcha.ts
+++ src/frontend/lib/api/captcha.ts
@@ -4,5 +4,5 @@ import axios from "axios";
 export type Captcha = { image: string; token: string };
 
 export async function getCaptcha(): Promise<Captcha> {
-  return (await axios.get(getApiUrl("captcha"))).data.data.captcha;
+  return (await axios.get(getApiUrl("/captcha"))).data.data.captcha;
 }
