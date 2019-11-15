fd3ade4aa1911dd113ed40e07679865f334d0efc
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-15 20:37:09 +0100

    implement stop session button

--- src/frontend/lib/components/content/toolbar/right/OwnerActionMenu.tsx
+++ src/frontend/lib/components/content/toolbar/right/OwnerActionMenu.tsx
@@ -1,15 +1,68 @@
+import { deleteSession } from "../../../../api/session";
 import { useMenuHandler } from "../../../../hooks/menuHandler";
+import { UserRole } from "../../../../models/UserRole";
+import { setSession, setUserRole, showSnackbar } from "../../../../store/actions/global";
 import { selectSession } from "../../../../store/selectors/global";
-import { Hidden, IconButton, Menu, MenuItem, Typography } from "@material-ui/core";
+import { TextDialog } from "../../../layout/dialogs/TextDialog";
+import { Button, IconButton, Menu, MenuItem } from "@material-ui/core";
 import { MoreVert } from "@material-ui/icons";
+import { useRouter } from "next/router";
 import * as React from "react";
+import { useModal } from "react-modal-hook";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "StoreTypes";
 
 type Props = ConnectedProps<typeof connectToRedux>;
 
-const InternalOwnerActionMenu: React.FunctionComponent<Props> = props => {
-  if (!props.session) return null;
+const InternalOwnerActionMenu: React.FunctionComponent<Props> = ({
+  session,
+  setSession,
+  setUserRole,
+  showSnackbar,
+}) => {
+  if (!session) {
+    return null;
+  }
+  const router = useRouter();
+
+  const [showDeleteSessionModal, hideDeleteSessionModal] = useModal(({ in: open, onExited }) => (
+    <TextDialog
+      open={open}
+      onExited={onExited}
+      onClose={hideDeleteSessionModal}
+      dialogId="stop-session"
+      dialogTitle="Stop session and exit?"
+      dialogText="This will irreversibly delete the session and all its associated statistics. Are you sure?"
+      buttons={
+        <>
+          <Button onClick={hideDeleteSessionModal} color="primary" autoFocus>
+            No, take me back!
+          </Button>
+          <Button
+            onClick={() => {
+              hideDeleteSessionModal();
+              stopSessionAndExit();
+            }}
+            color="primary"
+            variant="contained"
+          >
+            Yes, delete the session
+          </Button>
+        </>
+      }
+    />
+  ));
+
+  const stopSessionAndExit = async () => {
+    if (await deleteSession(session)) {
+      showSnackbar("Session deleted");
+    } else {
+      showSnackbar("Sorry, the session could not be deleted.");
+    }
+    setUserRole(UserRole.Visitor);
+    setSession(null);
+    router.push("/");
+  };
 
   const [buttonProps, menuProps, createCloseHandler] = useMenuHandler("Session options");
 
@@ -29,8 +82,11 @@ const InternalOwnerActionMenu: React.FunctionComponent<Props> = props => {
           horizontal: "right",
         }}
       >
-        <MenuItem onClick={createCloseHandler(() => {})}>Action 1</MenuItem>
-        <MenuItem onClick={createCloseHandler(() => {})}>Action 2</MenuItem>
+        <MenuItem onClick={createCloseHandler(() => {})}>Invite members</MenuItem>
+        {/* <MenuItem onClick={createCloseHandler(() => {})}>Connect another device</MenuItem> */}
+        <MenuItem onClick={createCloseHandler(showDeleteSessionModal)}>
+          Stop session and exit
+        </MenuItem>
       </Menu>
     </>
   );
@@ -40,7 +96,13 @@ const mapStateToProps = (state: RootState) => ({
   session: selectSession(state),
 });
 
-const connectToRedux = connect(mapStateToProps);
+const mapDispatchToProps = {
+  setSession,
+  setUserRole,
+  showSnackbar,
+};
+
+const connectToRedux = connect(mapStateToProps, mapDispatchToProps);
 
 /**
  * A menu with session owner actions. Note: This component can only be rendered when the user is a
