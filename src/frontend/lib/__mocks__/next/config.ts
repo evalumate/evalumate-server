9a5fe1d5017d1de3eb0479be4e8a109fe32a2226
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-16 13:10:44 +0100

    mock next/config

---
+++ src/frontend/lib/__mocks__/next/config.ts
@@ -0,0 +1,5 @@
+import config from "../../../next.config";
+
+// Mock default export to return the appropriate config values (exported from the `next.config.js`
+// for convenience)
+export default () => config.config;
--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,22 +1,27 @@
 const withPlugins = require("next-compose-plugins");
 const config = require("config"); // Server config access
 
+const serverRuntimeConfig = {
+  port: config.get("port"),
+};
+
+const publicRuntimeConfig = {
+  captchaSolutionLength: config.get("captcha.solutionLength"),
+  sessionIdLength: config.get("session.idLength"),
+  sessionNameMaxLength: config.get("session.nameMaxLength"),
+  memberPingInterval: config.get("member.pingInterval"),
+  recordInterval: config.get("record.interval"),
+  historyScaleChangeInterval: config.get("client.historyScaleChangeInterval"),
+};
+
 const configuration = {
   distDir: "../../dist/frontend",
   webpack: (config, options) => {
     return config;
   },
-  serverRuntimeConfig: {
-    port: config.get("port"),
-  },
-  publicRuntimeConfig: {
-    captchaSolutionLength: config.get("captcha.solutionLength"),
-    sessionIdLength: config.get("session.idLength"),
-    sessionNameMaxLength: config.get("session.nameMaxLength"),
-    memberPingInterval: config.get("member.pingInterval"),
-    recordInterval: config.get("record.interval"),
-    historyScaleChangeInterval: config.get("client.historyScaleChangeInterval"),
-  },
+  serverRuntimeConfig,
+  publicRuntimeConfig,
 };
 
 module.exports = withPlugins([], configuration);
+module.exports.config = { serverRuntimeConfig, publicRuntimeConfig };
