ebe1d66d714c8f0b33f5b4c23888263b8fab699c
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-17 12:05:21 +0100

    fix RootState type

--- src/frontend/lib/store/types.d.ts
+++ src/frontend/lib/store/types.d.ts
@@ -3,6 +3,6 @@ declare module "StoreTypes" {
   import { StateType, ActionType } from "typesafe-actions";
   import { Store } from "redux";
   export type RootAction = ActionType<typeof import("./actions").default>;
-  export type RootState = StateType<ReturnType<typeof import("./reducers").default>>;
+  export type RootState = StateType<typeof import("./reducers").default>;
   export type Store = Store<RootState, RootAction>;
 }
