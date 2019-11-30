38bb2607fb5d2dabd08512d926306d732cbfe477
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-30 22:24:47 +0100

    implement session joining by url

--- src/frontend/pages/client/[sessionId].tsx
+++ src/frontend/pages/client/[sessionId].tsx
@@ -1,27 +1,64 @@
+import { UnderstandingBulb } from "../../lib/components/content/UnderstandingBulb";
 import { Page } from "../../lib/components/layout/Page";
-import { setSession, setUserRole } from "../../lib/store/actions/global";
+import { setSession, setUserRole, showSnackbar } from "../../lib/store/actions/global";
 import { selectSession, selectUserRole } from "../../lib/store/selectors/global";
+import { NextPage } from "next";
 import * as React from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "StoreTypes";
-import { UnderstandingBulb } from "../../lib/components/content/UnderstandingBulb";
-import { NextPage } from "next";
+import { UserRole } from "../../lib/models/UserRole";
+import { getSession, joinSession } from "../../lib/api/session";
+import { setUnderstanding, setMember } from "../../lib/store/actions/member";
+import { JoinSessionForm } from "../../lib/components/forms/JoinSessionForm";
 
-type InitialProps = {};
+type InitialProps = {
+  showCaptchaForm: boolean;
+};
 
 type Props = InitialProps & ConnectedProps<typeof connectToRedux>;
 
 const ClientPage: NextPage<Props, InitialProps> = props => {
-  const title = props.session ? props.session.name : "Invalid session";
+  if (!props.session) {
+    return null;
+  }
+
   return (
-    <Page title={title} maxWidth="sm">
-      <UnderstandingBulb />
+    <Page title={props.session.name} maxWidth="sm">
+      {props.showCaptchaForm && <JoinSessionForm session={props.session} />}
+      {!props.showCaptchaForm && <UnderstandingBulb />}
     </Page>
   );
 };
 
-ClientPage.getInitialProps = async ({ store }) => {
-  return {};
+ClientPage.getInitialProps = async ({ store, query }) => {
+  let showCaptchaForm: boolean = false;
+
+  if (selectUserRole(store.getState()) === UserRole.Visitor) {
+    // A visitor wants to join a session
+    const sessionId = query.sessionId as string;
+    const session = await getSession(sessionId);
+
+    if (!session) {
+      store.dispatch(showSnackbar("Invalid session"));
+      // TODO redirect?
+    } else {
+      store.dispatch(setSession(session));
+
+      if (session.captchaRequired) {
+        // Visitor has to enter a captcha to join the session
+        showCaptchaForm = true;
+      } else {
+        // Visitor joins session
+        // TODO implement sagas for session joining and leaving + error handling
+        const member = await joinSession(session.id);
+        store.dispatch(setMember(member));
+        store.dispatch(setUnderstanding(true));
+        store.dispatch(setUserRole(UserRole.Member));
+      }
+    }
+  }
+
+  return { showCaptchaForm };
 };
 
 const mapStateToProps = (state: RootState) => ({
