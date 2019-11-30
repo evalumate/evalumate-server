91d704d69022d3a6bd91e53a149bdafe100753ed
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-30 22:25:05 +0100

    add forms to the homepage

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,9 +1,29 @@
+import { JoinSessionForm } from "../lib/components/forms/JoinSessionForm";
 import { Page } from "../lib/components/layout/Page";
-import * as React from "react";
+import { Paper } from "../lib/components/layout/Paper";
+import { Grid, Typography } from "@material-ui/core";
 import { NextPage } from "next";
+import * as React from "react";
+import { CreateSessionForm } from "../lib/components/forms/CreateSessionForm";
 
 const HomePage: NextPage<{}, void> = () => {
-  return <Page title="EvaluMate – Feedback Made Easy" titleAddHomepageTitle={false}></Page>;
+  return (
+    <Page title="EvaluMate – Feedback Made Easy" titleAddHomepageTitle={false} maxWidth="sm">
+      <Grid item xs={12}>
+        <Paper>
+          <JoinSessionForm />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} container justify="center">
+        <Typography>or</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <CreateSessionForm />
+        </Paper>
+      </Grid>
+    </Page>
+  );
 };
 
 export default HomePage;
