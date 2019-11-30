3c210d6651e1aa4c984eb85236b6c98fac276ad6
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-30 22:25:21 +0100

    add coming soon message to the about page

--- src/frontend/pages/about.tsx
+++ src/frontend/pages/about.tsx
@@ -1,9 +1,16 @@
 import { Page } from "../lib/components/layout/Page";
 import { NextPage } from "next";
 import * as React from "react";
+import { Typography, Grid } from "@material-ui/core";
 
 const AboutPage: NextPage<{}, void> = () => {
-  return <Page title="About"></Page>;
+  return (
+    <Page title="About">
+      <Grid item xs={12} container justify="center">
+        <Typography>Coming soon</Typography>
+      </Grid>
+    </Page>
+  );
 };
 
 export default AboutPage;
