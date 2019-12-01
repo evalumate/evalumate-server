972d4e3dbc5be0b8676b4444455980ae744849b9
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-12-01 17:37:34 +0100

    use appropriate drawer menu list icons

--- src/frontend/lib/components/content/toolbar/left/MenuDrawerContent.tsx
+++ src/frontend/lib/components/content/toolbar/left/MenuDrawerContent.tsx
@@ -1,17 +1,17 @@
 import { LinkedListItem } from "../../../layout/toolbar/LinkedListItem";
 import { Divider, List } from "@material-ui/core";
-import { Mail as MailIcon } from "@material-ui/icons";
+import { PersonAdd, Create, Info } from "@material-ui/icons";
 import * as React from "react";
 
 export const MenuDrawerContent: React.FunctionComponent = () => (
   <>
     <List>
-      <LinkedListItem icon={MailIcon} text="Join session" href="/client" />
-      <LinkedListItem icon={MailIcon} text="Create session" href="/master" />
+      <LinkedListItem icon={PersonAdd} text="Join session" href="/client" />
+      <LinkedListItem icon={Create} text="Create session" href="/master" />
     </List>
     <Divider />
     <List>
-      <LinkedListItem icon={MailIcon} text="About" href="/about" />
+      <LinkedListItem icon={Info} text="About" href="/about" />
     </List>
   </>
 );
