7dd3deff24641fbdac5649c0f1bc42a752a3aafa
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-12-01 17:48:39 +0100

    fix Store declaration

--- src/frontend/lib/store/types.d.ts
+++ src/frontend/lib/store/types.d.ts
@@ -1,8 +1,8 @@
 // Redux-store-related type definitions
 declare module "StoreTypes" {
   import { StateType, ActionType } from "typesafe-actions";
-  import { Store } from "redux";
+  import { Store as ReduxStore } from "redux";
   export type RootAction = ActionType<typeof import("./actions").default>;
   export type RootState = StateType<typeof import("./reducers").default>;
-  export type Store = Store<RootState, RootAction>;
+  export type Store = ReduxStore<RootState, RootAction>;
 }
