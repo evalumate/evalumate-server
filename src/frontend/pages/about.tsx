1de452c2498bb0de90f4b3b6a4288dc17a84753b
robinibor <23406021+robinibor@users.noreply.github.com>
2020-06-25 11:20:12 +0200

    added language files and content for 'About' page. Modified source code of 'About' page

--- src/frontend/pages/about.tsx
+++ src/frontend/pages/about.tsx
@@ -3,12 +3,52 @@ import { NextPage } from "next";
 import * as React from "react";
 
 import { Page } from "../lib/components/layout/Page";
+import { TitleSubtitleBox } from "../lib/components/layout/TitleSubtitleBox";
+import { useTranslation } from "../lib/util/i18n";
 
 const AboutPage: NextPage<{}, void> = () => {
+  const { t } = useTranslation(["about"]);
+
   return (
     <Page title="About">
+      <Grid item xs={12}>
+        <TitleSubtitleBox />
+      </Grid>
+      <Grid item xs={12}>
+        <hr />
+      </Grid>
       <Grid item xs={12} container justify="center">
-        <Typography>Coming soon</Typography>
+        <Typography>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: t("problem_text", {
+                interpolation: { escapeValue: false },
+              }),
+            }}
+          />
+          <br />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: t("solution_text", {
+                interpolation: { escapeValue: false },
+              }),
+            }}
+          />
+          <br />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: t("advantage_text", {
+                interpolation: { escapeValue: false },
+              }),
+            }}
+          />
+          <br />
+          <center>
+            <i>Button design in progress...</i>
+          </center>
+          <br />
+          <br />
+        </Typography>
       </Grid>
     </Page>
   );
